73fd2851f64c5142b434051888659c27d812b03a
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2022-06-21 15:43:53 +0100

    Made some things into prefabs and started working on generation

---
+++ Assets/Scripts/Environment/WorldGenerator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour { // This generates a ChunkData which is then rendered by ChunkRenderer
+	[SerializeField] private float chunkSize;
+	[SerializeField] private int generationBoxWidth; // In chunks
+
+	public class ChunkData {
+
+	}
+	//private Hashtable 
+}
